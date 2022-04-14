8456aeb13a9334de78fcd2e2e8b0965a9167b62b
KimYunBeom <yunbeom.kim@bespinglobal.com>
2022-04-14 15:51:29 +0900

    zip

--- gui_project/practice.py
+++ gui_project/practice.py
@@ -3,9 +3,21 @@
 # lst.reverse()
 # print(lst)
 
-lst2 = [1, 2, 3, 4, 5]
-print('리스트 2 뒤집기 전 :', lst2)
+# lst2 = [1, 2, 3, 4, 5]
+# print('리스트 2 뒤집기 전 :', lst2)
 
-lst3 = reversed(lst2)
-print('리스트 2 뒤집은 후 :', lst2) # 원본은 뒤집히지 않음
-print('리스트 3 :', list(lst3)) # 새 변수에 할당하면 뒤집힘
+# lst3 = reversed(lst2)
+# print('리스트 2 뒤집은 후 :', lst2) # 원본은 뒤집히지 않음
+# print('리스트 3 :', list(lst3)) # 새 변수에 할당하면 뒤집힘
+
+
+kor = ['사과', '바나나', '오렌지']
+eng = ['apple', 'banana', 'orange']
+print(list(zip(kor, eng)))
+
+mixed = [('사과', 'apple'), ('바나나', 'banana'), ('오렌지', 'orange')]
+print(list(zip(*mixed)))
+
+kor2, eng2 = zip(*mixed)
+print(kor2)
+print(eng2)
