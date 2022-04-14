609f0dbbfc88c03c7031f5291c78bfdd8b848d73
KimYunBeom <yunbeom.kim@bespinglobal.com>
2022-04-14 14:24:09 +0900

    저장 경로

--- gui_project/2_basic_function.py
+++ gui_project/2_basic_function.py
@@ -1,17 +1,18 @@
 from tkinter import *
 from tkinter import filedialog # 별도 명시적인 import 필요
 import tkinter.ttk as ttk
+import tkinter.messagebox as msgbox
 
 root = Tk()
 root.title('Nado GUI')
-root.geometry('640x480+800+50')
 
 # 파일 추가
 def add_file():
-  files = filedialog.askopenfilenames(title='이미지 파일을 선택하세요', filetypes=( ('JPG 파일', '*.jpg'), ('PNG 파일', '*.png'), ('BMP 파일', '*.bmp'), ('모든 파일', '*.*') ), initialdir=r'c:\data\dev\workspace3\pythongui\gui_project\images') # 최초에 사용자가 지정한 경로를 보여줌
+  files = filedialog.askopenfilenames(title='이미지 파일을 선택하세요', filetypes=( ('JPG 파일', '*.jpg'), ('모든 파일', '*.*') ), initialdir=r'c:\data\dev\workspace3\pythongui\gui_project\images') # 최초의 사용자가 지정한 경로를 보여줌
 
   # 사용자가 선택한 파일 목록
   for file in files:
+    # print(file)
     list_file.insert(END, file)
 
 # 선택 삭제
@@ -19,6 +20,33 @@ def del_file():
   # print(list_file.curselection())
   for index in reversed(list_file.curselection()):
     list_file.delete(index)
+
+# 저장 경로 (폴더)
+def browse_dest_path():
+  folder_selected = filedialog.askdirectory()
+  if folder_selected == '': # 사용자가 취소를 누를 때
+    return
+  # print(folder_selected)
+  txt_dest_path.delete(0, END)
+  txt_dest_path.insert(0, folder_selected)
+
+# 시작
+def start():
+  # 각 옵션들 값을 확인
+  print('가로넓이 :', cmb_width.get())
+  print('간격     :', cmb_space.get())
+  print('포맷     :', cmb_format.get())
+
+  # 파일 목록 확인
+  if list_file.size() == 0:
+    msgbox.showwarning('경고', '이미지 파일을 추가하세요')
+    return
+  
+  # 저장 경로 확인
+  if len(txt_dest_path.get()) == 0:
+    msgbox.showwarning('경고', '저장 경로를 선택하세요')
+    return
+
 
 # 파일 프레임 (파일 추가, 선택 삭제)
 file_frame = Frame(root)
@@ -48,7 +76,7 @@ path_frame.pack(fill='x', padx=5, pady=5, ipady=5)
 txt_dest_path = Entry(path_frame)
 txt_dest_path.pack(side='left', fill='x', expand=True, padx=5, pady=5, ipady=4) # 높이 변경
 
-btn_dest_path = Button(path_frame, text='찾아보기', width=10)
+btn_dest_path = Button(path_frame, text='찾아보기', width=10, command=browse_dest_path)
 btn_dest_path.pack(side='right', padx=5, pady=5)
 
 # 옵션 프레임
@@ -103,7 +131,7 @@ frame_run.pack(fill='x', padx=5, pady=5)
 btn_close = Button(frame_run, padx=5, pady=5, text='닫기', width=12, command=root.quit)
 btn_close.pack(side='right', padx=5, pady=5)
 
-btn_start = Button(frame_run, padx=5, pady=5, text='시작', width=12)
+btn_start = Button(frame_run, padx=5, pady=5, text='시작', width=12, command=start)
 btn_start.pack(side='right', padx=5, pady=5)
 
 # 창 사이즈 변경 불가
